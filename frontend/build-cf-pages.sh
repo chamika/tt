72f253f90e0e0e5e500a4c8a75c5aac9f00c78e2
Chamika <chamikaw@gmail.com>
2025-12-31 00:52:33 +0000

    Fix git branches with '\' characters

--- frontend/build-cf-pages.sh
+++ frontend/build-cf-pages.sh
@@ -1,7 +1,7 @@
 if [ "$CF_PAGES_BRANCH" = "main" ]; then
   export VITE_API_URL="https://tabletennis.chamika.workers.dev/api"
 else
-  export VITE_API_URL="https://$CF_PAGES_BRANCH-tabletennis.chamika.workers.dev/api"
+  export VITE_API_URL="https://${CF_PAGES_BRANCH//\//-}-tabletennis.chamika.workers.dev/api"
 fi
 echo "VITE_API_URL=$VITE_API_URL"
 
