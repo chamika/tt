b8bb775f1373cacbd20837ff9d860645c01a2497
Chamika <chamikaw@gmail.com>
2025-12-31 00:34:44 +0000

    Update build-cf-pages script to support prod deployment

--- frontend/build-cf-pages.sh
+++ frontend/build-cf-pages.sh
@@ -1,4 +1,8 @@
-export set VITE_API_URL="https://$CF_PAGES_BRANCH-tabletennis.chamika.workers.dev/api" 
+if [ "$CF_PAGES_BRANCH" = "main" ]; then
+  export VITE_API_URL="https://tabletennis.chamika.workers.dev/api"
+else
+  export VITE_API_URL="https://$CF_PAGES_BRANCH-tabletennis.chamika.workers.dev/api"
+fi
 echo "VITE_API_URL=$VITE_API_URL"
 
 npm install
